5de898c978da3b38a3f4f53fc9bd288e1a690d69
Mohammed Talha Sheikh <p14165162@myemail.dmu.ac.uk>
2017-11-28 10:37:50 +0000

    JSON data is being displayed on page

---
+++ javascript/parse-data.js
@@ -0,0 +1,39 @@
+(function(){
+  let demoData = document.getElementById("demo");
+
+  // creates a JavaScript object by parsing the JSON-formatted string from the file
+  let obj = JSON.parse(jsonString);
+  //console.log(obj);
+//
+  /*
+    EXERCISE 01:
+    Rewrite this to make it *loop* through all the lecturers' names.
+
+	Clue: use "for key in..." and try to work out the syntax
+  */
+let theData = "";
+
+  for (key in obj.lecturers){
+
+     theData += `<li>${obj.lecturers[key].firstName} ${obj.lecturers[key].lastName}</li>`;
+     
+
+  }
+  //  (Note the ES6 'template literal' syntax - you can add HTML tags)
+
+
+  // set theData here to an empty string
+
+  // create a loop around this line with "for ... in" (look it up)_
+
+  // end the for loop here
+
+  // populates the chosen element's content with the data:
+
+demoData.innerHTML = theData;
+  /*
+    EXERCISE 02:
+    Once you have looped through all the data, present the
+    results on the page, styled with CSS.
+  */
+}());
